2feb295ce92cdda8509ce0922ffcddbbe2c46309
Aayu231 <aayushi.sarda@gmail.com>
2021-03-31 23:49:05 +0530

    new_startup

--- Django/authentication/models.py
+++ Django/authentication/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class new_startup():
+    #details of the registrar
+    founder_name = models.CharField(max_length=50)
+    founder_pan_id = models.CharField(max_length=20)
+    #company details
+    registration_no = models.CharField(max_length=20)
+    company_name = models.CharField(max_length=50)
+    industry = models.CharField(max_length=50)
+    sector = models.CharField(max_length=50)
+    team_size = models.IntegerField()
+    company_code = models.IntegerField() #this will be used as team-code
+
+
+
+
