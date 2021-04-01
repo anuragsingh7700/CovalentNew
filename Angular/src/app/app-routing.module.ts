f72faa10a6ece1774965ba3a5b58273f72238e91
HarshSingh74 <harshsatishsingh1174@gmail.com>
2021-04-01 15:24:08 +0530

    Frontend
    frontend

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExploreComponent } from './explore/explore.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {ExtDashboardComponent } from './ext-dashboard/ext-dashboard.component' ;
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'explore', component: ExploreComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'ext-dashboard', component: ExtDashboardComponent},
+  // {path: '', component: HomeComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
